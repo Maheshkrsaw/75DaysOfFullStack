ea75d75af72aa022c01075a9216abbfb21e239dc
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-09-04 19:18:24 +0530

    adbavnce of use state stuff complete

--- 23_reactday-3/src/components/Card.jsx
+++ 23_reactday-3/src/components/Card.jsx
@@ -1,19 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Card() {
-    const [val ,setval] = useState({name:"mahesh" , isBanned:false})
-
+  const [val, setval] = useState({ name: "mahesh", isBanned: false });
 
   return (
-
-    <div className='px-2 py-4 '>
-        <h1> name :{val.name}</h1>
-        <h2>banned : {val.isBanned.toString()}</h2>
-        <button onClick={ ()=> setval({...val , isBanned:!val.isBanned}) } className='px-2 py-2 bg-blue-500 rounded-full text-sm mt-2  text-white'>Change </button>
-        </div>
-
-    
-  )
+    <div className="px-2 py-4 ">
+      <h1> name :{val.name}</h1>
+      <h2>banned : {val.isBanned.toString()}</h2>
+      <button
+        onClick={() => setval({ ...val, isBanned: !val.isBanned })}
+        className={`px-2 py-2 ${
+          val.isBanned ? "bg-blue-500" : "bg-red-500"
+        } rounded-full text-sm mt-2  text-white`}
+      >
+        Change{" "}
+      </button>
+    </div>
+  );
 }
 
-export default Card
+export default Card;

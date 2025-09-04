28e6ba56be409a4b3222c1e1eac61272d4bb9803
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-09-04 20:43:05 +0530

    ye wala array usestate ka part2 h>

--- 23_reactday-3/src/components/Card.jsx
+++ 23_reactday-3/src/components/Card.jsx
@@ -6,7 +6,7 @@ const [val,setval]=useState([1,2,3,4,5])
   return (
     <div className='p-4'>
      {val.map(item => <h1>{item}</h1>)}
-     <button onClick={ ()=> setval (()=> val.filter ((elem,index)=> index != val.length-1))}
+     <button onClick={ ()=> setval (()=> val.filter ((item ,index) => item%2 !==0))}
      className='px-2 py-1 text-white rounded-full bg-sky-300 text-x '>click</button>
     </div>
   )

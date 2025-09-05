1566dd801c208efb8dc754155024e17317d1482c
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-09-05 07:12:44 +0530

    23 day complete

--- 23_reactday-3/src/components/Card.jsx
+++ 23_reactday-3/src/components/Card.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react'
 
 function Card() {
-const [val,setval]=useState([1,2,3,4,5])
+
+      const[val , setval ] = useState([
+        { name: "Mahesh", age: 22 },
+    { name: "Amit", age: 24 },
+    { name: "Neha", age: 21 }])
 
   return (
-    <div className='p-4'>
-     {val.map(item => <h1>{item}</h1>)}
-     <button onClick={ ()=> setval (()=> val.filter ((item ,index) => item%2 !==0))}
-     className='px-2 py-1 text-white rounded-full bg-sky-300 text-x '>click</button>
+    <div>
+      {val.map(elem => 
+       <>
+        <h1>{elem.name}</h1>
+        <h2>{elem.age}</h2>
+       </>
+      )}
+
+      <button onClick={()=> setval(()=> val.map(item => item.name== "Mahesh" ? ({name: "Mahesh", age: 23}) : item  ))}
+      className='px-2 py-2 text-white bg-blue-500 rounded-full'>click</button>
     </div>
   )
 }

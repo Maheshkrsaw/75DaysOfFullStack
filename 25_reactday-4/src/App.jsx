37a96ab5f95f98380ee517fb7c2dacc2fd64e1a1
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-09-06 14:58:37 +0530

    props wala completed

---
+++ 25_reactday-4/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Card from './components/card'
+
+function App() {
+
+const data = [
+  { name : "Mahesh" , profession : "coder" , image:"https://images.unsplash.com/photo-1621179314026-0f23b2835c1e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D" },
+  { name : "sohan" , profession : "artist" , image:"https://plus.unsplash.com/premium_photo-1732117940281-e1598445016c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D" },
+  { name : "rohan" , profession : "painter" , image:"https://images.unsplash.com/photo-1558357896-5e2eb7aff45b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fHw%3D" },
+  { name : "mohan" , profession : "graphics" , image:"https://images.unsplash.com/photo-1548842149-376b275ff705?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8" },
+]
+
+
+  return (
+    <>
+       <div className='w-full h-screen bg-zinc-200 flex items-center gap-4 justify-center'>
+      {
+        data.map((item,index)=>(
+          <Card image={item.image} name={item.name} profession={item.profession} />
+        ))
+      }
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ 25_reactday-4/src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function card({image , name ,profession}) {
+  return (
+    <div className='w-53 bg-white rounded-md overflow-hidden'>
+      <div className='w-full h-42 bg-sky-500'>
+        <img className='w-full h-full object-top object-cover' src={image} alt="" />
+      </div>
+      <div className='w-full p-3 font-semibold'>
+        <h3 className='text-2xl'>{name}</h3>
+        <h5>{profession}</h5>
+        <button className=' mt-2 px-3 py-1 text-xs text-white bg-blue-500 font-semibold rounded-md'>Add Friend </button>
+      </div>
+    </div>
+  )
+}
+
+export default card

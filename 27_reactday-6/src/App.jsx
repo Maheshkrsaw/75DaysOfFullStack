24cc4b59375ffb0a84e7642060d4bfa5c610dfe6
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-09-08 19:08:31 +0530

    readme done

---
+++ 27_reactday-6/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='w-full h-screen bg-zinc-200 flex items-center justify-center'>
+      <div className='container mx-auto'>
+        hello
+      </div>
+      </div>
+  )
+}
+
+export default App 

9a32a57a3a613c6ee813b13bf5e6282ba2c8b2e4
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-09-08 08:33:24 +0530

    using react controlled done and now begins react form hook started

--- 26_react-day5/src/App.jsx
+++ 26_react-day5/src/App.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 
 function App() {
-  const[val , setval]=useState({name : ""})
+  const[val , setval]=useState({name : "" , email :""})
+  const handelSubmit =(dets)=>{
+    dets.preventDefault();
+  }
   
   return (
     <div>
-      <form action="">
-        <input onChange={(event)=>setval({name:event.target.value})} type="text" placeholder='Enter your name ' />
+      <form action="" onSubmit={handelSubmit}>
+        <input onChange={(event)=>setval({...val,name:event.target.value})} type="text" placeholder='Enter your name ' />
+        <input onChange={(event)=>setval({...val,email:event.target.value})} type="email" placeholder='Email ' />
+        <input type="submit" />
       </form>
     </div>
   )

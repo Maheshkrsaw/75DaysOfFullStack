baab32d7477ebbf1090566d530234b3a2eae1d05
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-09-09 11:47:11 +0530

    done with classname and span tag or inkine navlink

--- 28_react-day7/src/components/Nav.jsx
+++ 28_react-day7/src/components/Nav.jsx
@@ -1,31 +1,48 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
     <>
-    <nav className='flex items-center justify-between p-5 '>
-        <h1 className='font-bold text-2xl'>Mahesh</h1>
-        <div className='flex gap-10 font-semibold'>
-          <NavLink style={(e)=>{
-            return {
+      <nav className="flex items-center justify-between p-5 ">
+        <h1 className="font-bold text-2xl">Mahesh</h1>
+        <div className="flex gap-10 font-semibold">
+          <NavLink
+            to="/"
+            style={(e) => ({
+              color: e.isActive ? "#FFA2A2" : "black",
+              fontWeight: e.isActive ? "bold" : "normal",
+            })}
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            style={(e) => {
+              return {
                 color: e.isActive ? "tomato" : "",
-            }
-          }}  to="/">Home</NavLink>
-          <NavLink style={(e)=>{
-            return {
-                color: e.isActive ? "tomato" : "",
-            }
-          }} to="/users">Users</NavLink>
-          <NavLink style={(e)=>{
-            return {
-                color: e.isActive ? "tomato" : "",
-            }
-          }} to="/about">About</NavLink>
+                fontWeight: e.isActive ? "bold" : "",
+              };
+            }}
+            to="/users"
+          >
+            Users
+          </NavLink>
+          <NavLink
+            className={(e) => {
+              return [
+                e.isActive ? "text-red-300" : "",
+                e.isActive ? "font-bold" : "",
+              ].join(" ");
+            }}
+            to="/about"
+          >
+            About
+          </NavLink>
         </div>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;

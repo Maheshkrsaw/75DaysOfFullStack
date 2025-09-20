8326b7aeb989fe62a379dba129f57595fa3b29f3
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-09-21 05:03:57 +0530

    are bhai ek error ko slove kartr karte 30 min lag gye av sab fix h isaliye commit kar raha

---
+++ 39,40_redux/src/Store/reducers/ProductReducers.jsx
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+const initialState={
+    
+}
+
+export const ProductSlice=createSlice({
+    name:"product",
+    initialState,
+    reducers:{
+
+    }
+})
+
+export default ProductSlice.reducer;

dd3373773223ad23cd27683e17fcc8057118d061
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-08 10:08:39 +0530

    trying to fix the netlify deployment

--- 41,42,43,44,45,46_Movieapp-project/src/main.jsx
+++ 41,42,43,44,45,46_Movieapp-project/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { store } from './Store/Store';
 import { Provider } from 'react-redux'
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -9,8 +8,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
  <Provider store={store}>
- <BrowserRouter>
+   <BrowserRouter>
       <App />
-  </BrowserRouter>
+   </BrowserRouter>
  </Provider>
 )

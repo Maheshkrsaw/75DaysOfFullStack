437f59dd292419f585af4eae824faaca86711a8b
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-17 19:19:35 +0530

    sign in , signup , functionalties done

--- 60_AUTHECATION-RELATED/index.js
+++ 60_AUTHECATION-RELATED/index.js
@@ -1,8 +1,50 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 app.use(express.json());
 
-const users=[];
+const users = [];
+
+function generateToken() {
+  let options = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "K",
+    "L",
+    "M",
+    "N",
+    "D",
+    "P",
+    "0",
+    "R",
+    "S",
+    "U",
+    "U",
+    "V",
+    "w",
+    "x",
+    "Y",
+    "2",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  let token = "";
+  for (let i = 0; i < 132; i++) {
+    token = token + options[Math.floor(Math.random() * options.length)];
+  }
+  return token;
+}
 
 app.post("/signup",(req,res)=>{
     const username=req.body.username;
@@ -18,11 +60,25 @@ app.post("/signup",(req,res)=>{
     })
 })
 
-app.post("/signin",(req,res)=>{
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-})
+  // find user from the list
+  const foundUser = users.find(
+    (u) => u.username === username && u.password === password
+  );
 
+  if (foundUser) {
+    const token =generateToken();
+    foundUser.token=token;
+    res.json({ message: "✅ Sign-in successful!" , token });
+  } else {
+    res.status(401).json({ message: "❌ Invalid username or password" });
+  }
+}); 
 
 app.listen(3000, () => {
   console.log("🚀 Server running on http://localhost:3000");
 });
+ 

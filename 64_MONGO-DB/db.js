4201a84b0539d0a55b43a12d4866a557482dc452
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-22 13:30:01 +0530

    fix complete /signup, signin ,auth implwemtaion prper work as well as todo

--- 64_MONGO-DB/db.js
+++ 64_MONGO-DB/db.js
@@ -1,12 +1,13 @@
+// db.js
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const User = new Schema({
-  name: String,
-  email: String,
-  password: String
+    name: String,
+    email: String,
+    password: String
 });
 
 const Todo = new Schema({
@@ -21,4 +22,4 @@ const TodoModel = mongoose.model('todos', Todo);
 module.exports = {
     UserModel,
     TodoModel
-}
+};
--- 64_MONGO-DB/index.js
+++ 64_MONGO-DB/index.js
@@ -1,49 +1,92 @@
 const express = require("express");
 const { UserModel, TodoModel } = require("./db");
+// const { auth, JWT_SECRET } = require("./auth");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 
 const app = express();
-const jwt_SECRET = "Mahesh123";
+const JWT_SECRET = "Mahesh123";
 
 app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(
-  "mongodb+srv://Mahesh:MaheshMahi@cluster0.cjdwieg.mongodb.net/testing"
-);
+    "mongodb+srv://Mahesh:MaheshMahi@cluster0.cjdwieg.mongodb.net/testing"
+)
+.then(() => console.log("✅ MongoDB Connected"))
+.catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
 // Signup route
 app.post("/signup", async function (req, res) {
-  const { email, password, name } = req.body;
+    const { email, password, name } = req.body;
 
-  await UserModel.create({
-    email: email,
-    password: password,
-    name: name,
-  });
+    await UserModel.create({
+        email: email,
+        password: password,
+        name: name,
+    });
 
-  res.json({
-    message: "You are signed up",
-  });
+    res.json({
+        message: "You are signed up",
+    });
 });
 
 // Signin route
-app.post("/signin", function (req, res) {
-  // Signin logic goes here
+app.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await UserModel.findOne({
+        email: email,
+        password: password,
+    });
+
+    if (user) {
+        const token = jwt.sign(
+            { id: user._id.toString() },
+            JWT_SECRET
+        );
+
+        res.json({
+            token: token,
+        });
+    } else {
+        res.status(403).json({
+            message: "Incorrect credentials",
+        });
+    }
 });
 
 // Add a new todo
-app.post("/todo", function (req, res) {
-  // Todo creation logic goes here
+app.post("/todo", auth, function (req, res) {
+    const userId = req.userId;
+    res.json({
+        userId: userId
+    });
 });
 
 // Get all todos
-app.get("/todos", function (req, res) {
-  // Fetch todos logic goes here
+app.get("/todos", auth, function (req, res) {
+    const userId = req.userId;
+    res.json({
+        userId: userId
+    });
 });
+
+// Auth middleware
+function auth(req, res, next) {
+    const token = req.headers.token;
+    const decodeddata = jwt.verify(token, JWT_SECRET);
+    if (decodeddata) {
+        req.userId = decodeddata.id;
+        next();
+    } else {
+        res.status(403).json({
+            message: "Incorrect credentials"
+        });
+    }
+}
 
 // Start server
 app.listen(3000, () => {
-  console.log("🚀 Server running on http://localhost:3000");
+    console.log("🚀 Server running on http://localhost:3000");
 });

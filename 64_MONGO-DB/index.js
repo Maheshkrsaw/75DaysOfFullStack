1c8688d95d5c201e3b00545a5b461f59abb339e9
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-10-22 11:45:29 +0530

    till now signup process got all work perfectly

--- 64_MONGO-DB/index.js
+++ 64_MONGO-DB/index.js
@@ -1,20 +1,49 @@
-const express = require ('express')
+const express = require("express");
+const { UserModel, TodoModel } = require("./db");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
 const app = express();
+const jwt_SECRET = "Mahesh123";
 
+app.use(express.json());
 
-app.post("/signup",(req,res)=>{
-    
-})
-app.post("/signin",(req,res)=>{
+// Connect to MongoDB
+mongoose.connect(
+  "mongodb+srv://Mahesh:MaheshMahi@cluster0.cjdwieg.mongodb.net/testing"
+);
 
-})
-app.post("/todo",(req,res)=>{
+// Signup route
+app.post("/signup", async function (req, res) {
+  const { email, password, name } = req.body;
 
-})
-app.get("/todo",(req,res)=>{
+  await UserModel.create({
+    email: email,
+    password: password,
+    name: name,
+  });
 
-})
+  res.json({
+    message: "You are signed up",
+  });
+});
 
+// Signin route
+app.post("/signin", function (req, res) {
+  // Signin logic goes here
+});
+
+// Add a new todo
+app.post("/todo", function (req, res) {
+  // Todo creation logic goes here
+});
+
+// Get all todos
+app.get("/todos", function (req, res) {
+  // Fetch todos logic goes here
+});
+
+// Start server
 app.listen(3000, () => {
   console.log("🚀 Server running on http://localhost:3000");
 });
